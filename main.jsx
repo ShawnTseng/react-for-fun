f4bea26a2dad81f5ead9f6d930f0d8f5801ff5a5
shawntseng <shawntseng@digiwin.biz>
2018-11-28 23:43:08 +0800

    建立redux的store

---
+++ main.jsx
@@ -0,0 +1,14 @@
+const { createStore, combineReducers } = Redux;
+const { TodoApp, reducers } = window.App;
+
+// 1. 將 reducers 集合物件轉換成一個 reducer 函數
+const composedReducer = combineReducers(reducers);
+// 2. 使用 reducer 函數，建立 Store 實例，Store 會將改變狀態邏輯委託給 reducer 實作
+const store = createStore(composedReducer);
+
+// 3. 將原本 index.html 中的程式移來這裡
+const { TodoAppContainer } = window.App;
+ReactDOM.render(
+    <TodoAppContainer />,
+    document.getElementById('app')
+);
